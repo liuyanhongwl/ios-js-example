dcc4e51f5aa6ae320e63637c3f53d99edddbf70b
刘艳红 <758038069@qq.com>
2016-01-29 21:25:22 +0800

    Initial Commit

---
+++ HHOC-JS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHOC-JS
+//
+//  Created by Hong on 16/1/29.
+//  Copyright © 2016年 Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
